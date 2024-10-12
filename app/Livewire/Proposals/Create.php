0654716a5204c07d4a1acebaa577560de3e99b48
dayviso06 <dayvison.rocha@hotmail.com>
2024-10-12 14:25:15 -0300

    add:salvar proposta no banco

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -3,21 +3,35 @@
 namespace App\Livewire\Proposals;
 
 use App\Models\Project;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Create extends Component
 {
     public Project $project;
-    public bool $modal = true;
+    public bool $modal = false;
+
+    #[Rule('required', 'email')]
     public string $email = '';
+    #[Rule('required', 'numeric', 'min:1')]
     public int $hours = 0;
+
+    public bool $agree = false;
+
     public function save()
     {
+        $this->validate();
+
+        if(!$this->agree) {
+            $this->addError('agree', 'Please agree to accept our terms and conditions.');
+            return;
+        }
+
         $this->project->proposals()
-            ->create([
-                'email' => $this->email,
-                'hours' => $this->hours,
-            ]);
+            ->updateOrCreate(['email' => $this->email],
+                ['hours' => $this->hours]);
+
+        return  redirect()->route('projects.show', $this->project)->with('success', 'Proposta enviada com sucesso!');
     }
     public function render()
     {
--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -1,5 +1,11 @@
 <div class="col-span-2">
 
+    @if(session('success'))
+        <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 rounded-lg">
+            {{ session('success') }}
+        </div>
+    @endif
+
     <x-projects.card :$project />
 
 </div>
