ee41951900e3cf5307655b43114189d467c3998f
dayviso06 <dayvison.rocha@hotmail.com>
2024-10-13 22:21:25 -0300

    adicionado tempo decorrente da proposta

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,11 +3,19 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()->orderByDesc('hours')
+            ->get();
+    }
 
     public function render()
     {
